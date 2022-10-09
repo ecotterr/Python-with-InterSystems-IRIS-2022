1ca3eb6f02b1198ea4893ea7980a182c25cfb72a
ISCecotterr <elijah.cotterrell@intersystems.com>
2022-10-09 05:35:00 +0100

    Python Gateway and fixed CSV import

--- src/Python/gateway/Crypto.py
+++ src/Python/gateway/Crypto.py
@@ -1,9 +1,8 @@
 class coin:
     def __init__(self):
-        self.Date = "2022-10-07"
+        self.Date = "07/10/2022"
         self.Open = 0.441948
         self.High = 0.470297
         self.Low = 0.433151
         self.Close = 0.467157
-        self.Volume = 0.467157
-        self.MarketCap = 2,368,317,679
+        self.Volume = 2368317679
